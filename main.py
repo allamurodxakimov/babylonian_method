426c038b3381e21dea91ac5f9587f214ce934a43
allamurodxakimov <145457030+allamurodxakimov@users.noreply.github.com>
2023-09-21 04:12:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def main(S, d):
-    '''create a babylonian function.
-    
-    Args:
-        S (int): number
-        d (int): numnber
-        
-    Returns:
-        float: result
-    '''
-    return
+S = int(input())
+d = int(input())
+a = (S-pow(d,2))/(2*d)
+b = a+d
+x = b - pow(a,2)/(2*b)
+print(x)
